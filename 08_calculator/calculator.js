772bf20adae3198bc24397602f05a4805d6cab91
Oussama Oumlil <hamza.oumlil.5@gmail.com>
2023-06-16 20:51:08 +0100

    finished the 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (!array.length) return 0;
+	let ret = array[0];
+	for (let index = 1; index < array.length; index++) {
+	  ret += array[index];
+	}
+	return ret;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+	let ret = args[0];
+	for (let index = 1; index < args.length; index++) {
+		ret *= args[index];
+	}
+	return ret;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+	let ret = 1;
+	while (num) {
+		ret *= num;
+		num--;
+	}
+	return ret;
 };
 
 // Do not edit below this line
